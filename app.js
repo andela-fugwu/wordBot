9dd059cba8325e2246927af278d8638e896a5211
andela-fugwu <ugwu.nnaemekan@gmail.com>
2015-01-09 12:02:33 +0100

    fixed origin request

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var app = express();
 
-require('./models/db');
-var words = require('./routes/words');
-
-app.use('/', words);
-
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS');
 
     next();
 });
+
+require('./models/db');
+var words = require('./routes/words');
+
+app.use('/', words);
+
+
 app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
